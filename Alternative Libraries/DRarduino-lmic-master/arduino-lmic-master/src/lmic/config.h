c8ccadbfa1e63c59757a858adfcb179225637edf
Kevin Barrell <kevin_barrell@bigpond.com>
2020-08-30 15:47:27 +1000

    Re-download Dragino Lora Lmic Master for clean start

--- Alternative Libraries/DRarduino-lmic-master/arduino-lmic-master/src/lmic/config.h
+++ Alternative Libraries/DRarduino-lmic-master/arduino-lmic-master/src/lmic/config.h
@@ -6,23 +6,20 @@
 // compiler commandline, use this file instead.
 
 
-//#define CFG_eu868 1
-#define CFG_us915 1
-//#define CFG_au915 1
+#define CFG_eu868 1
+//#define CFG_us915 1
+//#define CFG_au921 1
 //#define CFG_as923 1
 //#define CFG_in866 1
 
-#define LG02_LG01 1       // Using LG01N single channel gateway
+//#define LG02_LG01 1
 
-//AU915: DR_SF12=0, DR_SF11=1, DR_SF10=2, DR_SF9=3, DR_SF8=4, DR_SF7=5
-//       DR_SF8C=6, DR_SF12C=8, DR_SF11C=9, DR_SF10C=10, DR_SF9C=11, DR_SF8C=12
-//       DR_SF7C=13
 //US915: DR_SF10=0, DR_SF9=1, DR_SF8=2, DR_SF7=3, DR_SF8C=4 
 //       DR_SF12CR=8, DR_SF11CR=9, DR_SF10CR=10, DR_SF9CR=11, DR_SF8CR=12, DR_SF7CR
 #if defined(CFG_us915) && defined(LG02_LG01)
-// CFG_us915 || CFG_as923 || CFG_au915
-#define LG02_UPFREQ   916800000
-#define LG02_DNWFREQ  916800000
+// CFG_us915 || CFG_as923 
+#define LG02_UPFREQ   902320000
+#define LG02_DNWFREQ  923300000
 #define LG02_RXSF     3      // DR_SF7  For LG01/LG02 Tx
 #define LG02_TXSF     8      // DR_SF12CR For LG02/LG02 Rx
 #elif defined(CFG_eu868) && defined(LG02_LG01)
