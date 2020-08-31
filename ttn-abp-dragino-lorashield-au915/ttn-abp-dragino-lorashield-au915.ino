9849c0102cf5f022036135f63b9314d037d4ca55
Kevin Barrell <kevin.barrell@barrellacumen.com>
2020-09-01 00:31:05 +1000

    Corrected Shield RX2 DR to SF7BW500

--- ttn-abp-dragino-lorashield-au915/ttn-abp-dragino-lorashield-au915.ino
+++ ttn-abp-dragino-lorashield-au915/ttn-abp-dragino-lorashield-au915.ino
@@ -255,7 +255,7 @@ void setup() {
 
     // TTN uses SF9 for its RX2 window.
     //LMIC.dn2Dr = DR_SF9;
-	LMIC.dn2Dr = DR_SF7;
+	LMIC.dn2Dr = DR_SF7CR;
 
     // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
     LMIC_setDrTxpow(DR_SF7,14);
